5aa0a85ad4ac0d2765b415d61a9b1c3310bc46fa
StephanXu <xuzihanapple@live.com>
2020-03-02 20:59:51 +0800

    修正默认参数

--- Logger/Logger/Services/ArgumentService.cs
+++ Logger/Logger/Services/ArgumentService.cs
@@ -47,7 +47,7 @@ namespace Logger.Services
                                     ""stopBit"": ""OneStopBit"",
                                     ""synchronize"": true,
                                     ""syncInterval"": 1
-                                },
+                                    },
                                 ""camera"": {
                                     ""deviceIndex"": 0,
                                     ""frameWidth"": 1280,
@@ -57,35 +57,55 @@ namespace Logger.Services
                                     ""filename"": ""test.avi""
                                 },
                                 ""aimbot"": {
-                                    ""maxShootRadius"": 120,
                                     ""enemyColor"": ""Red"",
+
                                     ""brightness"": 30,
                                     ""thresColor"": 50,
-                                    ""lightBarMinArea"": 15,
+
+                                    ""lightBarMinArea"": 15.0,
                                     ""lightBarContourMinSolidity"": 0.5,
                                     ""lightBarEllipseMinAspectRatio"": 1.8,
+
+
                                     ""armorMaxAngleDiff"": 7.5,
                                     ""armorMaxHeightDiffRatio"": 0.6,
-                                    ""armorMaxYDiffRatio"": 2,
+                                    ""armorMaxYDiffRatio"": 2.0,
                                     ""armorMinXDiffRatio"": 0.5,
+
                                     ""armorBigArmorRatio"": 4.2,
-                                    ""armorSmallArmorRatio"": 2,
-                                    ""armorMinAspectRatio"": 1,
+                                    ""armorSmallArmorRatio"": 2.0,
+                                    ""armorMinAspectRatio"": 1.0,
                                     ""armorMaxAspectRatio"": 5.5,
-                                    ""areaNormalizedBase"": 1000,
-                                    ""sightOffsetNormalizedBase"": 200
+
+                                    ""areaNormalizedBase"": 1000.0,
+                                    ""sightOffsetNormalizedBase"": 200.0
                                 },
                                 ""poseSolver"": {
-                                    ""offsetX"": 0,
-                                    ""offsetZPitch"": 131,
-                                    ""offsetYPitch"": 50,
-                                    ""offsetYYaw"": -37,
-                                    ""offsetZYaw"": 213,
-                                    ""offsetYaw"": 0,
-                                    ""offsetPitch"": 0.55,
-                                    ""initV"": 20,
+                                                ""cameraToGimbalX"": 0.0,
+                                    ""cameraToGimbalY"": -40.7,
+                                    ""cameraToGimbalZ"": 141.1,
+                                    ""barrelToGimbalY"": 40.1,
+                                    ""rotOverlapLen"": 100000.0,
+
+                                    ""initV"": 20.0,
                                     ""initK"": 0.026,
-                                    ""gravity"": 9.8
+                                    ""gravity"": 9.8,
+                                    ""scaleDist"": 1.2
+                                },
+                                ""chassis"": {
+                                    ""PIDWheelSpeed_Kp"": 15000,
+                                    ""PIDWheelSpeed_Ki"": 10,
+                                    ""PIDWheelSpeed_Kd"": 0,
+                                    ""PIDWheelSpeed_Th_Out"": 16000,
+                                    ""PIDWheelSpeed_Th_IOut"": 2000,
+
+                                    ""PIDChassisAngle_Kp"": 40,
+                                    ""PIDChassisAngle_Ki"": 0,
+                                    ""PIDChassisAngle_Kd"": 0,
+                                    ""PIDChassisAngle_Th_Out"": 6,
+                                    ""PIDChassisAngle_Th_IOut"": 0.2,
+
+                                    ""TOP_WZ"": 3
                                 }
                             }"
             });
