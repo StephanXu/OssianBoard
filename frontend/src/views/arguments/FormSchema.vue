6f234f995d59bb18751d2bdbef87cf6e3981d80a
StephanXu <xuzihanapple@live.com>
2020-09-22 20:55:40 +0800

    增加FormSchema

---
+++ frontend/src/views/arguments/FormSchema.vue
@@ -0,0 +1,34 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "FormSchema",
+  props: {
+    value: {
+      type: Object,
+      default: () => {
+      }
+    },
+    schema: {
+      type: Object,
+      default: () => {
+      }
+    }
+  },
+  data() {
+    return {
+    }
+  },
+  methods:{
+  },
+  async created() {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
